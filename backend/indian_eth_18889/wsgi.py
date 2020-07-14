4c42755bdbabd02e45f5e4dd837649d9fff7aec2
Crowdbotics <team@crowdbotics.com>
2020-07-14 10:27:16 +0000

    Update models

--- backend/indian_eth_18889/wsgi.py
+++ backend/indian_eth_18889/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'indian_eth_18889.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "indian_eth_18889.settings")
 
 application = get_wsgi_application()
